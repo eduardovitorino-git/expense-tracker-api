987404fa76190089ffdb72423eab4661d286232c
Eduardo Vitorino <eduardovitorino.contato@gmail.com>
2025-11-06 19:14:59 -0300

    Spring MVC

--- app/src/main/java/com/expensetracker/app/controller/ExpenseController.java
+++ app/src/main/java/com/expensetracker/app/controller/ExpenseController.java
@@ -1,30 +1,52 @@
 package com.expensetracker.app.controller;
 
-import java.util.List;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.expensetracker.app.entity.Expense;
-import com.expensetracker.app.service.ExpenseService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+//@RequestMapping("/api")
 public class ExpenseController {
 	
-	private ExpenseService service;
-	@SuppressWarnings("unused")
-	private ObjectMapper obj;
+//	private ExpenseService service;
+//	@SuppressWarnings("unused")
+//	private ObjectMapper obj;
+//	
+//	public ExpenseController(ExpenseService expenseService, ObjectMapper objectMapper) {
+//		this.service = expenseService;
+//		this.obj = objectMapper;
+//	}
+//	
+//	@GetMapping("/expenses")
+//	public List<Expense> getAll() {
+//		return service.findAll();
+//	}
 	
-	public ExpenseController(ExpenseService expenseService, ObjectMapper objectMapper) {
-		this.service = expenseService;
-		this.obj = objectMapper;
+//	@GetMapping("/hello")
+//	public String getHello(Model theModel) {
+//		theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+//		return "hello";
+//	}
+	
+	// Model is for sharing data between controller and view
+	@GetMapping("/expenseForm")
+	public String showForm(Model model) {
+		model.addAttribute("expense", new Expense());
+		return "expense-form";
 	}
 	
-	@GetMapping("/expenses")
-	public List<Expense> getAll() {
-		return service.findAll();
+//	@GetMapping("/processForm")
+//	public String processForm() {
+//		return "hello";
+//	}
+	
+	// HttpServletRequest is a object for the HTML elements
+	@PostMapping("/processForm")
+	public String processForm(@ModelAttribute("expense") Expense expense) {
+		return "expense-confirmation";
 	}
 }
